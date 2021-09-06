e7a5de955b8e898c4dac8bface89603c8edb89ce
RmnSnk <romain.sonneck@gmail.com>
2021-09-06 21:12:49 +0200

    début du code pour tester si l'utilisateur et la bdd existe

--- main.py
+++ main.py
@@ -8,17 +8,57 @@ postgres VIA l'utilisateur postgres.
 
 import psutil
 
-l = []
-for proc in psutil.process_iter(['pid', 'name', 'username']):
-    if proc.name() == 'postgres':
-        l.append(proc)
 
-if len(l) == 0:
-    print("Le serveur Postgresql n'est pas lancé")
+class Config:
+    """Classe qui contient la configuration"""
+
+    # Nom du processus postgre recherché
+    nom_proc_pg = "postgres"
+
+    # Nom de l'utilisateur postgre et de sa base de donnée
+    nom_utilisateur_pg = "python_project"
+    nom_bdd_pg = "python_project"
+
+
+class CheckPostgre:
+    """
+    Classe pour vérifier le service postgre :
+    1 - Le serveur postgre tourne bien
+    2 - L'utilisateur et la base de donnée exite.
+    Si un de ces tests échoue il faut prévenir le sysadmin pour configurer la bdd
+    """
+
+
+    def postgre_isrunning():
+
+        """Renvoie une liste contenant les différents processus postgre si le service postgre est lancé, sinon renvoie 0"""
+
+        l = []
+        for proc in psutil.process_iter(['pid', 'name', 'username']):
+            if proc.name() == Config.nom_proc_pg:
+                l.append(proc)
+
+        if len(l) == 0:
+            return False
+        else:
+            return True
+
+
+    def user_isexist():
+        pass
+
+    def db_isexist():
+        pass
+
+
+
+# TODO : prévoir des logs pour enregistrer les test
+
+# Script de test
+
+if CheckPostgre.postgre_isrunning == True:
+    print(True)
 else:
-    print("Le serveur Posgresql est lancé via les processus")
-    for proc in l:
-        print(f"{proc.pid} | utilisateur : {proc.username()}")
+    print(False)
 
-#TODO : maintenant que l'on a compris comment ça marche : faire un classe et un methode qui renvoie True or False
-#TODO : prévoir des logs
+
